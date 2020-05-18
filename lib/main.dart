0e6223f11d35ebce4ff7bb8e7f83184ebadf9ee6
Sam1414 <samarthagarwal1414@gmail.com>
2020-05-18 23:26:01 +0530

    quiz

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'quizBrain.dart';
 
 void main() => runApp(Quizzler());
 
@@ -25,6 +26,26 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  QuizBrain quizBrain = QuizBrain();
+  List<Icon> scoreKeeper = [];
+  void evaluate(bool buttonValue) {
+    bool answer = quizBrain.getAnswer();
+    setState(() {
+      bool isNextQuestion = quizBrain.nextQuestion();
+      if (buttonValue == answer && isNextQuestion == true) {
+        scoreKeeper.add(Icon(
+          Icons.check,
+          color: Colors.green,
+        ));
+      } else if (buttonValue != answer && isNextQuestion == true) {
+        scoreKeeper.add(Icon(
+          Icons.close,
+          color: Colors.red,
+        ));
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -37,7 +58,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                quizBrain.getQuestion(),
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -51,7 +72,7 @@ class _QuizPageState extends State<QuizPage> {
           child: Padding(
             padding: EdgeInsets.all(15.0),
             child: FlatButton(
-              textColor: Colors.white,
+              //textColor: Colors.white,
               color: Colors.green,
               child: Text(
                 'True',
@@ -62,6 +83,7 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked true.
+                evaluate(true);
               },
             ),
           ),
@@ -80,11 +102,15 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked false.
+                evaluate(false);
               },
             ),
           ),
         ),
         //TODO: Add a Row here as your score keeper
+        Row(
+          children: scoreKeeper,
+        ),
       ],
     );
   }
